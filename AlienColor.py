94f94b9bbfa1d0d16d74f7484f21e4309f6ebf2f
Clyreek <157425506+Clyreek@users.noreply.github.com>
2024-10-02 20:42:55 -0400

    Add files via upload

---
+++ AlienColor.py
@@ -0,0 +1,14 @@
+#Clyric Allen#
+
+print ("What color is the Alien?")
+Alien_color = input()
+
+#If function to determine the amount of points earned based on color#
+if Alien_color=="green":
+    print ("You earned 5 points.")
+
+elif Alien_color=="yellow":
+        print("You earned 10 points.")
+
+elif Alien_color=="red":
+            print ("You earned 15 points.")
---
+++ ElseVotingAge.py
@@ -0,0 +1,10 @@
+#Clyric Allen#
+
+Age = int( input("Please enter your age?"))
+
+if Age < 18:
+    print ("You are not old enough to vote yet. You must be 18 or older")
+
+else:
+    print("You are old enough to vote.")
+
